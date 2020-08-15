202cf0e88619e730bd11c47be41d38e11f62274a
Khris Parrales <lecristianot@gmail.com>
2020-08-14 21:36:20 -0500

    new header of ghpages for CORS

--- index.js
+++ index.js
@@ -27,6 +27,10 @@ app.use(function(req, res, next) {
     // Website you wish to allow to connect
     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4202');
     res.setHeader("Access-Control-Allow-Origin", "http://localhost:4200");
+    res.setHeader(
+        "Access-Control-Allow-Origin",
+        "https://khrisparrales.github.io/RoomMovie/"
+    );
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
