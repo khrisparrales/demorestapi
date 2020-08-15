b6eb7c6134e1ef0a5c41932bff6d462270ebfd4a
Khris Parrales <lecristianot@gmail.com>
2020-08-14 22:11:15 -0500

    new header khrisparrales for CORS try with utf

--- index.js
+++ index.js
@@ -22,29 +22,34 @@ mongo.connect(
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 // Add headers
+// app.use(function(req, res, next) {
+
+//     // Website you wish to allow to connect
+//     res.setHeader("Access-Control-Allow-Origin", 2, [
+//         "http://localhost:4202",
+//         "https://khrisparrales.github.io"
+//     ]);
+//     // res.setHeader("Access-Control-Allow-Origin", "http://localhost:4200");
+//     // res.setHeader(
+//     //     "Access-Control-Allow-Origin",
+
+//     // );
+//     // Request methods you wish to allow
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+//     // Request headers you wish to allow
+//     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+//     // Set to true if you need the website to include cookies in the requests sent
+//     // to the API (e.g. in case you use sessions)
+//     res.setHeader('Access-Control-Allow-Credentials', true);
+
+//     // Pass to next layer of middleware
+//     next();
+// });
+//try multiple headers
 app.use(function(req, res, next) {
-
-    // Website you wish to allow to connect
-    res.setHeader("Access-Control-Allow-Origin", [
-        "http://localhost:4202",
-        "https://khrisparrales.github.io"
-    ]);
-    // res.setHeader("Access-Control-Allow-Origin", "http://localhost:4200");
-    // res.setHeader(
-    //     "Access-Control-Allow-Origin",
-
-    // );
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
+    res.setHeader("charset", "utf-8");
     next();
 });
 
